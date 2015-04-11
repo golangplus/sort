6b15c4b0e5dc30e6716e9af9ec4c15b658284427
David Deng <daviddengcn@gmail.com>
2015-04-11 11:00:31 -0700

    Change testcase to examples

--- sort_test.go
+++ sort_test.go
@@ -1,14 +1,14 @@
 package sortp
 
 import (
+	"fmt"
 	"sort"
-	"testing"
 )
 
 // Make sure InterfaceStruct implements sort.Interface
 var _ sort.Interface = InterfaceStruct{}
 
-func TestSortF(t *testing.T) {
+func ExampleSortF() {
 	data := []int{5, 3, 1, 8, 0}
 
 	SortF(len(data), func(i, j int) bool {
@@ -16,8 +16,26 @@ func TestSortF(t *testing.T) {
 	}, func(i, j int) {
 		data[i], data[j] = data[j], data[i]
 	})
+	
+	fmt.Println(data)
+	// OUTPUT:
+	// [0 1 3 5 8]
+}
 
-	if !sort.IntsAreSorted(data) {
-		t.Errorf("Data is not sorted by SortF: %v", data)
-	}
+func ExampleInterfaceStruct() {
+	data := []int{5, 3, 1, 8, 0}
+
+	sort.Sort(InterfaceStruct{
+		LenF: func() int {
+			return len(data)
+		}, LessF: func(i, j int) bool {
+			return data[i] < data[j]
+		}, SwapF: func(i, j int) {
+			data[i], data[j] = data[j], data[i]
+		},
+	})
+	
+	fmt.Println(data)
+	// OUTPUT:
+	// [0 1 3 5 8]
 }
