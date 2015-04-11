154baa91138d76408b4aa0765eb149262a04249a
David Deng <daviddengcn@gmail.com>
2015-04-10 21:57:03 -0700

    Comments

--- sort.go
+++ sort.go
@@ -1,9 +1,9 @@
-// Copyright 2015 The Golangplus Authors. All rights reserved.
+// Copyright 2015 The Golang Plus Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 /*
-Package sortp is a plus to "sort" package.
+Package sortp is a plus to standard "sort" package.
 */
 package sortp
 
